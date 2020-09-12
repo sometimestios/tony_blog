a3b0ac7af489bb0af4c943f5f4f3ab561e1d624e
tony_honor <93711779@qq.com>
2020-09-12 17:26:29 +0800

    modified urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    post_list=Post.objects.all().order_by('-createdTime')
+    return render(request,'index.html',context={
+        'post_list':post_list,
+    })
