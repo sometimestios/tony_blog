bfaf407e0bc41dea8d55814ee69799df0c55fab4
tony_SCUlab <example@example.com>
2020-09-14 23:42:34 +0800

    developed sidebar

---
+++ blog/templatetags/blog_extra.py
@@ -0,0 +1,32 @@
+from django import template
+from ..models import Post, Category, Tag
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog/inclusions/_recent_posts.html')
+def show_recent_posts(num=5):
+    return {
+        'recent_post_list': Post.objects.all().order_by('-createdTime')[:num],
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_archive.html')
+def show_archive():
+    return {
+        'date_list': Post.objects.dates('createdTime', 'month', order='DESC'),
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_category.html')
+def show_category():
+    return {
+        'category_list': Category.objects.all()
+    }
+
+
+@register.inclusion_tag('blog/inclusions/_tag.html')
+def show_tag():
+    return {
+        'tag_list':Tag.objects.all()
+    }
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 import markdown
-
+import re
 
 # Create your views here.
 def index(request):
@@ -14,12 +14,15 @@ def index(request):
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     # 通过markdown函数将md语法的正文转成了html文本
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    post.body = md.convert(post.body)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
     return render(request, 'blog/detail.html', context={
         'post': post
     })
