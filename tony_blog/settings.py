08028656d4812ba37d8dd16f7e155c7de75a6dd4
tony_ali <tony@ali.com>
2020-10-26 09:26:37 +0800

    部署成功

--- tony_blog/settings.py
+++ tony_blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1!94fsmr)n)+m)!zikug-ju$roxckehf83wrz^l(6!d(y#mva-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.tonysongwb.top']
 
